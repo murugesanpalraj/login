8fd2e847bbcf8b4fbf18b0a9617137fbc92f3ae2
Murugesan <pmg.guru@gmail.com>
2024-04-10 08:45:22 +0530

    yml initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login
+
+<div>
+    <input type='text' placeholder='Email'/><br/>
+    <input type='text' placeholder='password'/><br/>
+    <button type='submit'>Login</button>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Login
